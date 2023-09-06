5f166305febcdbe0782767bbcacae57712376ba3
RaoAbdulSami <abdul.sami@DESKTOP-7MGAJRV>
2023-09-06 11:29:14 +0500

    Stop watch utility added, TextBox for Showing the output of sorts created and binded to ViewModel, Time for different Sorts is showing successfully, now need to test if the output is correct or not

--- SortingAlgos/ViewModels/mainDisplayViewModel.cs
+++ SortingAlgos/ViewModels/mainDisplayViewModel.cs
@@ -7,6 +7,7 @@ using System.ComponentModel;
 using System.Windows.Input;
 using SortingAlgos.RelayCommads;
 using SortingAlgos.Model;
+using System.Diagnostics;
 
 namespace SortingAlgos.ViewModels
 {
@@ -24,9 +25,13 @@ namespace SortingAlgos.ViewModels
         public ICommand selectionSort{ get; }
         public ICommand insertionSort { get; }
 
+        Stopwatch stopwatch;
+
         mainDisplayModel sortModel;
         public mainDisplayViewModel()
         {
+            stopwatch = new Stopwatch();
+            stopwatch.Reset();
             sortModel = new mainDisplayModel();
 
             generateRand    = new RelayCommand(generateRandomArray);
@@ -35,6 +40,10 @@ namespace SortingAlgos.ViewModels
             selectionSort   = new RelayCommand(doSelectionSort);
             insertionSort   = new RelayCommand(doInsertionSort);
         }
+
+        /// <summary>
+        /// //////////////////////////////////              Binding Properties Defined   ///////////////////////////////////////////////////////
+        /// </summary>
 
         private string arraySizeInput;
 
@@ -66,12 +75,41 @@ namespace SortingAlgos.ViewModels
             set { sortedArrayShow = value; OnPropertyChange(nameof(SortedArrayShow)); }
         }
 
+        private string bubbleTimeShow;
+
+        public string BubbleTimeShow
+        {
+            get { return bubbleTimeShow; }
+            set { bubbleTimeShow = value; OnPropertyChange(nameof(BubbleTimeShow)); }
+        }
+
+        private string selectionTimeShow;
+
+        public string SelectionTimeShow
+        {
+            get { return  selectionTimeShow; }
+            set {  selectionTimeShow = value; OnPropertyChange(nameof(SelectionTimeShow)); }
+        }
+
+        private string insertionTimeShow;
+
+        public string InsertionTimeShow
+        {
+            get { return insertionTimeShow; }
+            set { insertionTimeShow = value; OnPropertyChange(nameof(InsertionTimeShow)); }
+        }
+
+
+        /// <summary>
+        /// ////////////////////////////////////////////////////  Actions Defined //////////////////////////////////////////////////////////////////////
+        /// </summary>
         int[] mainArray;
         int arraySize;
+
         public void generateRandomArray()
         {
             arraySize = int.Parse(ArraySize);
-             mainArray = sortModel.generateRandomNumberArray(arraySize);
+            mainArray = sortModel.generateRandomNumberArray(arraySize);
             string arrayString = string.Join("  ", mainArray);
             RandomArrayShow = arrayString;
         }
@@ -79,21 +117,51 @@ namespace SortingAlgos.ViewModels
 
         public void doBubbleSort()
         {
+            long elapsedMicroSeconds = 0;
+            TimeSpan elapsedTime;
+
+            stopwatch.Start();
             int[] array = sortModel.doBubbleSort(mainArray, arraySize);
+            stopwatch.Stop();
+            elapsedTime = stopwatch.Elapsed;
+            elapsedMicroSeconds = elapsedTime.Ticks / 10;
+
+            BubbleTimeShow = elapsedMicroSeconds.ToString();
+            stopwatch.Reset();
             string arrayString = string.Join("  ",array);
             SortedArrayShow = arrayString;
         }
 
         public void doSelectionSort()
         {
+            long elapsedMicroSeconds =0;
+            TimeSpan elapsedTime;
+
+            stopwatch.Start();
             int[] array = sortModel.doSelectionSort(mainArray, arraySize);
+            stopwatch.Stop();
+            elapsedTime = stopwatch.Elapsed;
+            elapsedMicroSeconds = elapsedTime.Ticks / 10;
+
+            SelectionTimeShow = elapsedMicroSeconds.ToString();
+            stopwatch.Reset();
             string arrayString = string.Join("  ", array);
             SortedArrayShow = arrayString;
         }
 
         public void doInsertionSort()
         {
+            long elapsedMicroSeconds =0;
+            TimeSpan elapsedTime;
+
+            stopwatch.Start();
             int[] array = sortModel.doInsertionSort(mainArray, arraySize);
+            stopwatch.Stop();
+            elapsedTime = stopwatch.Elapsed;
+            elapsedMicroSeconds = elapsedTime.Ticks / 10;
+
+            InsertionTimeShow = elapsedMicroSeconds.ToString();
+            stopwatch.Reset();
             string arrayString = string.Join("  ", array);
             SortedArrayShow = arrayString;
         }
